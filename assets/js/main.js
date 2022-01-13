82102db6d482391fc6a3de62df7c2b080e0fc835
satyamsharma17 <satyamsharma1725@gmail.com>
2022-01-13 17:35:02 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,261 @@
+const navMenu = document.getElementById("nav-menu"),
+      navToggle = document.getElementById("nav-toggle"),
+      navClose = document.getElementById("nav-close")
+
+/* SHOW MENU */
+/* Validate if constant exists. */
+if(navToggle)
+{
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add("show-menu")
+  })
+}
+
+/* Menu Hidden */
+/* Validate if constant exists. */
+if(navClose)
+{
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove("show-menu")
+  })
+}
+
+/* Remove Menu Mobile */
+const navLinks = document.querySelectorAll(".nav-link")
+
+function linkAction()
+{
+  const navMenu = document.getElementById("nav-menu")
+  // When we click on each navigation link, we remove the show menu class.
+  navMenu.classList.remove("show-menu")
+}
+navLinks.forEach(n => n.addEventListener('click', linkAction))
+
+/* Change Background Header */
+function scrollHeader()
+{
+  const header = document.getElementById("header")
+  // When the scroll is greater than 80 viewport height, add the class scroll header to the tag header.
+  if(this.scrollY >= 80) header.classList.add("scroll-header"); else header.classList.remove("scroll-header")
+}
+window.addEventListener("scroll", scrollHeader)
+
+/* Testimonial Swiper */
+var swiper = new Swiper(".testimonial-wrapper", {
+    spaceBetween: 30,
+    loop: 'true',
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+/* Scroll Sections Active Link */
+
+// Get all sections that have an id defined.
+const sections = document.querySelectorAll("section[id]");
+
+// Add an event listener listening for scroll.
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter()
+{
+  // Get current scroll position.
+  let scrollY = window.pageYOffset;
+  // Now we loop through sections to get height, top and ID values for each.
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58,
+    sectionId = current.getAttribute("id");
+    /* - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it.
+    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector. */
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
+    {
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add("active-link")
+    }
+    else
+    {
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove("active-link")
+    }
+  })
+}
+
+/* Portfolio Item Filter */
+const filterContainer = document.querySelector(".portfolio-filter-inner"),
+      filterBtns = filterContainer.children,
+      totalFilterBtn = filterBtns.length,
+      portfolioItems = document.querySelectorAll(".portfolio-item"),
+      totalPortfolioItem = portfolioItems.length;
+      
+      for(let i=0; i<totalFilterBtn; i++)
+      {
+        filterBtns[i].addEventListener("click", function()
+        {
+          filterContainer.querySelector(".active").classList.remove("active");
+          this.classList.add("active");
+
+          const filterValue = this.getAttribute("data-filter");
+          for(let k=0; k<totalPortfolioItem; k++)
+          {
+            if(filterValue === portfolioItems[k].getAttribute("data-category"))
+            {
+              portfolioItems[k].classList.remove("hide");
+              portfolioItems[k].classList.add("show");
+            }
+            else
+            {
+              portfolioItems[k].classList.add("hide");
+              portfolioItems[k].classList.remove("show");
+            }
+            if(filterValue === "all")
+            {
+              portfolioItems[k].classList.remove("hide");
+              portfolioItems[k].classList.add("show");
+            }
+          }
+        })
+      }
+
+/* Theme/Display Customization */
+const theme = document.querySelector("#theme-button");
+const themeModal = document.querySelector(".customize-theme");
+const fontSizes = document.querySelectorAll('.choose-size span');
+const colorPalette = document.querySelectorAll(".choose-color span");
+var root = document.querySelector(":root");
+const Bg1 = document.querySelector(".bg-1");
+const Bg2 = document.querySelector(".bg-2");
+const Bg3 = document.querySelector(".bg-3");
+
+// Open Modal
+const openThemeModal = () => {
+  themeModal.style.display = 'grid';
+}
+// Close Modal
+const closeThemeModal = (e) => {
+  if(e.target.classList.contains('customize-theme'))
+  {
+    themeModal.style.display = 'none';
+  }
+}
+theme.addEventListener("click", openThemeModal);
+themeModal.addEventListener("click", closeThemeModal);
+
+/* Fonts */
+
+//Remove active class from spans or font size selectors.
+const removeSizeSelector = () => {
+  fontSizes.forEach(size => {
+    size.classList.remove("active");
+  })
+}
+fontSizes.forEach(size => {
+  size.addEventListener('click', () => {
+
+    removeSizeSelector();
+    let fontSize;
+    size.classList.toggle('active');
+    if(size.classList.contains('font-size-1'))
+    {
+      fontSize = '12px';
+    }
+    else if(size.classList.contains('font-size-2'))
+    {
+      fontSize = '14px';
+    }
+    else if(size.classList.contains('font-size-3'))
+    {
+      fontSize = '16px';
+    }
+    else if(size.classList.contains('font-size-4'))
+    {
+      fontSize = '18px';
+    }
+    // Change font size of the root html element. 
+    document.querySelector('html').style.fontSize = fontSize;
+
+  })
+})
+
+/* Primary Colors */
+
+// Remove active class from colors.
+const changeActiveColorClass = () => {
+  colorPalette.forEach(colorPicker => {
+    colorPicker.classList.remove('active');
+  })
+}
+colorPalette.forEach(color => {
+  color.addEventListener('click', () => {
+    let primaryHue;
+    changeActiveColorClass();
+
+    if(color.classList.contains('color-1'))
+    {
+      primaryHue = 252;
+    }
+    else if(color.classList.contains('color-2'))
+    {
+      primaryHue = 52;
+    }
+    else if(color.classList.contains('color-3'))
+    {
+      primaryHue = 352;
+    }
+    else if(color.classList.contains('color-4'))
+    {
+      primaryHue = 152;
+    }
+    else if(color.classList.contains('color-5'))
+    {
+      primaryHue = 202;
+    }
+    color.classList.add("active");
+    root.style.setProperty('--primary-color-hue', primaryHue);
+  })
+})
+
+/* Theme Background */
+let lightColorLightness;
+let whiteColorLightness;
+let darkColorLightness;
+
+// Change Background Color
+const changeBG = () => {
+  root.style.setProperty('--light-color-lightness', lightColorLightness);
+  root.style.setProperty('--white-color-lightness', whiteColorLightness);
+  root.style.setProperty('--dark-color-lightness', darkColorLightness);
+}
+Bg1.addEventListener('click', () => {
+  // Add active class.
+  Bg1.classList.add('active');
+  // Remove active class from the others.
+  Bg2.classList.remove('active');
+  Bg3.classList.remove('active');
+  // Remove customized changes from local storage.
+  window.location.reload();
+})
+Bg2.addEventListener('click', () => {
+  darkColorLightness = '95%';
+  whiteColorLightness = '20%';
+  lightColorLightness = '15%';
+
+  // Add active class.
+  Bg2.classList.add('active');
+  // Remove active class from the others.
+  Bg1.classList.remove('active');
+  Bg3.classList.remove('active');
+  changeBG();
+})
+Bg3.addEventListener('click', () => {
+  darkColorLightness = '95%';
+  whiteColorLightness = '10%';
+  lightColorLightness = '0%';
+
+  // Add active class.
+  Bg3.classList.add('active');
+  // Remove active class from the others.
+  Bg2.classList.remove('active');
+  Bg1.classList.remove('active');
+  changeBG();
+})
